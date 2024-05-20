6c856f6931974f330f58509fcd75dfa58d59126e
Matt <solomatias321@gmail.com>
2024-05-20 16:41:31 +0200

    nav and admin layout

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,20 @@
+import Nav, { NavLink } from "@/components/Nav";
+import React from "react";
+
+export default function AdminLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Nav>
+        <NavLink href="/admin">Dashboard</NavLink>
+        <NavLink href="/admin/products">Products</NavLink>
+        <NavLink href="/admin/users">Customers</NavLink>
+        <NavLink href="/admin/orders">Sales</NavLink>
+      </Nav>
+      <div className="container my-5">hhhhhhh{children}</div>
+    </>
+  );
+}
---
+++ src/app/admin/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AdminDashboard = () => {
+  return <div>AdminLayout</div>;
+};
+
+export default AdminDashboard;
