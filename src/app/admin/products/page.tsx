3e17a34bd9e5ebc5ab5cd72cc59a1b0f7ce868f4
Matt <solomatias321@gmail.com>
2024-05-23 20:05:31 +0200

    wip table on products

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import PageHeader from "../_components/PageHeader";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const AdminProductsPage = () => {
   return (
@@ -13,8 +20,30 @@ const AdminProductsPage = () => {
           <Link href="/admin/products/new">Add Product</Link>
         </Button>
       </div>
+      <ProductsTable />
     </>
   );
 };
 
 export default AdminProductsPage;
+
+function ProductsTable() {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-0">
+            <span className="sr-only">Available For Purchase</span>
+          </TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Price</TableHead>
+          <TableHead>Orders</TableHead>
+          <TableHead className="w-0">
+            <span className="sr-only">Actions</span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody></TableBody>
+    </Table>
+  );
+}
