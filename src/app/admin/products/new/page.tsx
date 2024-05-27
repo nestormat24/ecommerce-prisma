cd74eb3ce99c561da7cf4a68b58f844d13c9f999
Matt <solomatias321@gmail.com>
2024-05-27 19:55:59 +0200

    completing add product form

--- src/app/admin/products/new/page.tsx
+++ src/app/admin/products/new/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PageHeader from "../../_components/PageHeader";
+import ProductForm from "../_components/ProductForm";
 
 export default function NewProductPage() {
   return (
